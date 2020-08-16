8fbae66db4833b7eeac8a462b7ea4e60767c6822
John Stassen <john.stassen@gmail.com>
2020-08-16 00:46:29 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,7 +10,7 @@ def input_to_index(user_input)
   converted_input = user_input.to_i - 1
 end
 
-def move(board, position, character="X")
+def move(board, position, character  = "X")
   board [converted_input] = character
   board
 end
